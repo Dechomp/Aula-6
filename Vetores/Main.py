9696301939d03d23f788f085801d8cf91daad3dc
Dechomptile <juliodechomp@gmail.com>
2025-08-06 17:20:58 -0300

    Aulas

---
+++ Vetores/Main.py
@@ -0,0 +1,15 @@
+def main():
+    nome = input("Diigte o seu nome: ")
+    vet = [1, 2, 3, 4 ,5]
+    i = 0
+    while i < 5:
+        print(vet[i])
+
+        i+= 1
+    
+    
+
+
+
+    return 0
+main()
