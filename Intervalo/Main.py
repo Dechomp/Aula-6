a07ffdb1d0a70e0e6360b84b7197500cb2f5974b
Dechomptile <juliodechomp@gmail.com>
2025-08-06 16:32:41 -0300

    Código intervalo

---
+++ Intervalo/Main.py
@@ -0,0 +1,53 @@
+def main():
+    inicio = int (input ("Inicio: "))
+    fim = int ( input ("Fim: "))
+
+    primos = ""
+
+
+    if inicio < fim:
+        while inicio <= fim:
+            i = 2
+
+            while inicio > i:
+                if inicio % i == 0:
+                    break
+                i += 1
+            
+            if inicio == i and inicio > 1:
+                primos += "\n" + str(inicio)
+
+            inicio += 1
+    elif inicio > fim:
+        while inicio >= fim:
+
+            i = 2
+
+            while inicio != i and inicio > 1:
+                if inicio % i == 0:
+                    break
+                i += 1
+            
+            if inicio == i and inicio > 1:
+                primos += "\n" + str (inicio)
+            inicio -= 1
+    else:
+        i = 2
+
+        while inicio != i and inicio > 1:
+            if inicio % i == 0:
+                break
+            i += 1
+        
+        if inicio == i and inicio > 1:
+            primos += "\n" + str (inicio) 
+        else:
+            primos += "intervalo sem primos"    
+
+    print("Primos: ", primos)
+
+
+
+
+    return 0
+main()
